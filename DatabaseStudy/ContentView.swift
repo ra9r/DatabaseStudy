b2d0ec5500a983db79e58007e621b1f66ae2dbca
Rodney Aiglstorfer <r@a9r.me>
2022-09-25 09:31:27 +0800

    Initial Commit

---
+++ DatabaseStudy/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DatabaseStudy
+//
+//  Created by Rodney Aiglstorfer on 9/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DatabaseStudy/DatabaseStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatabaseStudyApp.swift
+//  DatabaseStudy
+//
+//  Created by Rodney Aiglstorfer on 9/25/22.
+//
+
+import SwiftUI
+
+@main
+struct DatabaseStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
