a48e753a6ccfc0b68a85e7950b122a499492812b
Rodney Aiglstorfer <r@a9r.me>
2022-09-25 18:30:31 +0800

    cleaned up

--- DatabaseStudy/ContentView.swift
+++ DatabaseStudy/ContentView.swift
@@ -9,27 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject var taskManager = TaskManager()
-//    @State private var showAddTaskView = false
     
     var body: some View {
         TaskListView()
             .environmentObject(taskManager)
-//        ZStack(alignment: .bottomTrailing) {
-//            TaskListView()
-//                .environmentObject(taskManager)
-//
-//            SmallAddButton()
-//                .padding()
-//                .onTapGesture {
-//                    showAddTaskView.toggle()
-//                }
-//        }
-//        .sheet(isPresented: $showAddTaskView) {
-//            AddTaskView()
-//                .presentationDetents([.medium])
-//                .environmentObject(taskManager)
-//        }
-//        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
     }
 }
 
